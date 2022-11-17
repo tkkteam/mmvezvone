36690754d6f5e42e4331095000a04e4deb85409d
tkkteam <lovebytumjaa@gmail.com>
2022-11-17 12:28:43 +0700

    fix app head

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,26 +1,35 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 import Script from "next/script";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Script
-        strategy="lazyOnload"
-        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID="G-HG67SWCXZS"}`}
-      />
+    <Script
+    strategy="lazyOnload"
+    src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS="G-HG67SWCXZS"}`}
+  />
 
-      <Script id="google-analytics-script" strategy="lazyOnload">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', '${process.env.NEXT_PUBLIC_GA_ID="G-HG67SWCXZS"}', {
+  <Script strategy="lazyOnload">
+    {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS=="G-HG67SWCXZS"}', {
           page_path: window.location.pathname,
-          });
-    `}
-      </Script>
+        });
+            `}
+  </Script>
+    <>
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=1"
+        />
+      </Head>
       <Component {...pageProps} />
+    </>
     </>
   );
 }
